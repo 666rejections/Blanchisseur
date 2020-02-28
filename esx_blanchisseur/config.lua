44922473a09c874cb392ac6b33f38dd25ca6b95c
CI-EVENTS <61483052+Grumpy-5murf@users.noreply.github.com>
2020-02-29 00:15:04 +0100

    Add files via upload

---
+++ esx_blanchisseur/config.lua
@@ -0,0 +1,17 @@
+Config					= {}
+Config.Locale			= 'fr'
+Config.Slice			= 10000
+Config.Percentage		= 75
+Config.MarkerSize   	= {x = 3.0, y = 3.0, z = 3.0}
+Config.MarkerType       = 2
+Config.MarkerColor      = {r = 100, g = 204, b = 100}
+Config.openHours		= 0
+Config.closeHours		= 8
+Config.Blip 			= true
+Config.Hours 			= false
+Config.Menu				= true
+Config.Bonus			= {min = 01, max = 05}
+
+Config.Zones = {
+	{x = -1121.852, y = -488.981, z = 54.831,percent=30},
+}
